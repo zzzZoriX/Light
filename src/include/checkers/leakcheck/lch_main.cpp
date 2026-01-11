bba01ec12a26684df2c49df951368ed902281da7
anyway <zorix228337@gmail.com>
2026-01-11 22:51:02 +0300

    wrote function to check a single file

--- src/include/checkers/leakcheck/lch_main.cpp
+++ src/include/checkers/leakcheck/lch_main.cpp
@@ -4,7 +4,7 @@ checker_result_t leak_check(ArgsInfo ainf) {
     if (ainf.fs._Nleak)
         return CHECKER_SUCCESS(Checker::LEAK_CHECKER);
 
-    checker_result_t res;
+    checker_result_t res{Checker::LEAK_CHECKER};
     res.checker_result.checker = Checker::LEAK_CHECKER;
     res.res_type = CheckerResultType::CHECKRES;
 
@@ -27,4 +27,75 @@ checker_result_t leak_check(ArgsInfo ainf) {
     }
 
     return res;
+}
+
+file_result_t leak_check_file(std::ifstream ifp, struct FlagsState fs) {
+    file_result_t flres;
+    const token_t tokens = tokenize(std::move(ifp));
+    std::map<std::string, lch_var_data> current_scope_mem_alloc;
+    std::string line;
+    unsigned long line_n = 1;
+
+    flres.flres_t = FileResultType::FLRES;
+    flres.have_errs = false;
+/*
+ * Why do I use a for instead of a for-each?
+ * I'll need to navigate through the list of tokens, and it is more convenient to do this using indexes
+ */
+    for (std::size_t i = 0; i < tokens.size();) {
+        line += tokens[i];
+
+        if (tokens[i] == "\n") {
+            line.clear();
+            ++line_n;
+            continue;
+        }
+
+        if (fs._cpp) {
+            if (CPP_IS_MEM_ALLOC_WORD(tokens[i])) {
+                while (tokens[++i] != ";")
+                    line += tokens[i];
+
+                current_scope_mem_alloc[lch_var_data::cpp_get_var_name(line, fs._cpp_20_on)] = lch_var_data(line, line_n, fs._cpp_20_on);
+            }
+            else if (CPP_IS_MEM_DEALLOC_WORD(tokens[i])) {
+                while (tokens[++i] != ";")
+                    line += tokens[i];
+
+                current_scope_mem_alloc.erase(lch_var_data::cpp_get_var_name(line, fs._cpp_20_on));
+            }
+        }
+        else {
+            if (C_IS_MEM_ALLOC_WORD(tokens[i])) {
+                while (tokens[++i] != ";")
+                    line += tokens[i];
+
+                current_scope_mem_alloc[lch_var_data::c_get_var_name(line)] = lch_var_data(line, line_n);
+            }
+            else if (C_IS_MEM_DEALLOC_WORD(tokens[i])) {
+                while (tokens[++i] != ";")
+                    line += tokens[i];
+
+                current_scope_mem_alloc.erase(lch_var_data::c_get_var_name(line));
+            }
+        }
+    }
+
+//  TODO: do errors creating for remaining memory leaks
+    if (!current_scope_mem_alloc.empty()) {
+        for (const auto&[_, snd] : current_scope_mem_alloc) {
+            flres.errs.push_back({
+                error_msg_t(
+                    "main()",
+                    snd.full_line,
+                    "potential memory leak",
+                    snd.line_number
+                ),
+                std::to_string(snd.line_number) + "|+ delete " + snd.var_name + ";"
+            });
+        }
+        flres.have_errs = true;
+    }
+
+    return flres;
 }
