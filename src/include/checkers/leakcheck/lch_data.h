548b1b5cb29d774c2102731eca116963c875b2dc
anyway <zorix228337@gmail.com>
2026-01-15 22:05:32 +0300

    add constructors for the scope data struct

--- src/include/checkers/leakcheck/lch_data.h
+++ src/include/checkers/leakcheck/lch_data.h
@@ -90,6 +90,19 @@ typedef struct smd_t {
 	std::vector<obj_mdata_t> alloc_objs_data;
 	smd_t head_scope,
 	      global_scope;
+	
+	smd_t() = default;
+
+
+	smd_t(const std::string& name, smd_t& head, smd_t& global) noexcept:
+		scope_name(name), head_scope(head), global_scope(global), alloc_objs_data() {}
+
+	smd_t(const std::string&& name, smd_t& head, smd_t& global) noexcept:
+		scope_name(name), head_scope(head), global_scope(global), alloc_objs_data() {}
+
+	
+	smd_t(smd_t& head, smd_t& global) noexcept:
+		scope_name(""), head_scope(head), global_scope(global), alloc_objs_data() {}
 } scope_mdata_t;
 
 #endif //LCH_DATA_H
